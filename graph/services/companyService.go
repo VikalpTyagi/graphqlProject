51b4b1c35d6181ff4fc419851b28f3c52b2575fc
tyagivikalp99@gmail.com <tyagivikalp99@gmail.com>
2023-11-01 13:29:15 +0530

    company creation logic defined

--- graph/services/companyService.go
+++ graph/services/companyService.go
@@ -7,14 +7,24 @@ import (
 
 func (s *DbConnStruct) CreateCompany(newComp model.NewCompany) (*model.Company, error) {
 	var job []*gormmodel.NewJob
+	for _,sJob := range newComp.Jobs{
+		nJob:= gormmodel.NewJob{
+			Title: sJob.Title,
+		}
+		job = append(job,&nJob)
+	}
 	comp := gormmodel.NewCompany{
 		Name: newComp.Name,
 		City: newComp.City,
-		Jobs: append(job,newComp.Jobs),
+		Jobs: job,
 	}
 	err := s.db.Create(&comp).Error
 	if err != nil {
 		return &model.Company{}, err
 	}
-	return comp, nil
+	return &model.Company{
+		CompanyID: &comp.CompanyID,
+		Name: comp.Name,
+		City: comp.City,
+	}, nil
 }
